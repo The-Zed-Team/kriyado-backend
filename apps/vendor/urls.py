518c63ca055af1f6f55f423eb8bac4b7f14e6a89
anirudh-mk <its.me.anirudh.mk@gmail.com>
2025-09-23 07:40:48 +0530

    feat(vendor) - vendor update api

--- apps/vendor/urls.py
+++ apps/vendor/urls.py
@@ -4,14 +4,33 @@ from . import views
 
 urlpatterns = [
     path("vendor/create/", views.VendorCreateAPIView.as_view(), name="vendor-create"),
+    path("vendors/update/", views.VendorUpdateAPIView.as_view(), name="vendor-update"),
     path(
         "vendor/onboarding-status/",
         views.VendorOnboardingStatusAPIView.as_view(),
         name="vendor-onboarding-status",
     ),
-    path("vendor/branches/", views.VendorBranchListAPIView.as_view(), name="branch-list"),
-    path("vendor/branches/create/", views.VendorBranchCreateAPIView.as_view(), name="branch-create"),
-    path("vendor/branches/<uuid:pk>/", views.VendorBranchDetailAPIView.as_view(), name="branch-detail"),
-    path("vendor/branches/<uuid:pk>/update/", views.VendorBranchUpdateAPIView.as_view(), name="branch-update"),
-    path("vendor/branches/<uuid:pk>/delete/", views.VendorBranchDeleteAPIView.as_view(), name="branch-delete"),
+    path(
+        "vendor/branches/", views.VendorBranchListAPIView.as_view(), name="branch-list"
+    ),
+    path(
+        "vendor/branches/create/",
+        views.VendorBranchCreateAPIView.as_view(),
+        name="branch-create",
+    ),
+    path(
+        "vendor/branches/<uuid:pk>/",
+        views.VendorBranchDetailAPIView.as_view(),
+        name="branch-detail",
+    ),
+    path(
+        "vendor/branches/<uuid:pk>/update/",
+        views.VendorBranchUpdateAPIView.as_view(),
+        name="branch-update",
+    ),
+    path(
+        "vendor/branches/<uuid:pk>/delete/",
+        views.VendorBranchDeleteAPIView.as_view(),
+        name="branch-delete",
+    ),
 ]
