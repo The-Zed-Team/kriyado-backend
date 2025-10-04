06f545dca15846ae63fa418c11d275e0c420a339
anirudh-mk <its.me.anirudh.mk@gmail.com>
2025-10-04 15:05:50 +0530

    fix(permission issue fixed)

--- apps/account/views.py
+++ apps/account/views.py
@@ -42,7 +42,7 @@ from core.authentication.firebase import firebase_client
 
 class FirebaseUserAuthenticationView(APIView):
     permission_classes = [AllowAny]
-
+    authentication_classes = []
     @transaction.atomic
     def post(self, request):
         request_serializer = FirebaseUserAuthenticationRequestSerializer(
